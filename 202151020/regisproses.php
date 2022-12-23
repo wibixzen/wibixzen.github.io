1acfb971c74c2721fa160517430c98973a1322c5
wibixzen <104005129+wibixzen@users.noreply.github.com>
2022-12-24 03:54:40 +0700

    Update regisproses.php

--- 202151020/regisproses.php
+++ 202151020/regisproses.php
@@ -6,16 +6,6 @@ $nama=$_POST['nama'];
 $password=$_POST['password'];
 $foto=$_POST['foto'];
 
-if ($nim== "") {
-	header("location:signup.php?nim=kosong");
-}elseif ($nama== ""){
-    header("location:signup.php?nama=kosong");
-}elseif ($password== ""){
-    header("location:signup.php?password=kosong");
-}elseif ($foto== ""){
-    header("location:signup.php?foto=kosong");
-}
-
 $query=mysqli_query ($conn,"insert into mahasiswa values('','$nim','$nama','$password','$foto')");
 
 header("location:dashboarduser.html");
